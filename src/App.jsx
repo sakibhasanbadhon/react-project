0dc8f93e669203ded573b48acbebf0ca19b45be6
sakib <badhonkhan2033@gmail.com>
2024-07-02 00:24:31 +0600

    item add in list and remove using useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import React, { useRef, useState } from 'react';
-import Hero from './assets/component/Hero';
+import { useState } from "react";
+
 
 const App = () => {
-  const [apiData, setApiData] = useState(null);
-  const displayData = useRef();
 
-  const fetchData = async () => {
-    const response = await fetch("https://dummyjson.com/products");
-    const data = await response.json();
-    setApiData(data);
-  };
+  let [list,setList]= useState([]);
+  let [item,setItem]= useState("");
 
-  const showData = () => {
-    if (displayData.current && apiData) {
-      displayData.current.innerText = JSON.stringify(apiData, null, 2);
-    }
-  };
+   const addToList = () =>{
+      list.push(item);
+      setList([...list])
+      
+   }
+
+   const removeItem= (index) =>{
+      list.splice(index,1);
+      setList([...list])
+
+   }
 
   return (
     <div>
-      <p ref={displayData}></p>
-      <button onClick={fetchData}>Call Api</button>
-      <button onClick={showData}>Show Data</button>
+      <table>
+        <tbody>
+          {
+            list.length !==0?(
+              list.map((element, index)=>{
+                return(
+                  <tr>
+                    <td>{element}</td>
+                    <td><button onClick={()=>{
+                      removeItem(index)
+                    }}>X</button></td>
+                  </tr>
+                )
+              })
+            ):(<tr></tr>)
+          }
+        </tbody>
+      </table>
+      <input onChange={(e)=>setItem(e.target.value)} type="text" placeholder="item"/>
+      <button onClick={addToList}> Add </button>
     </div>
   );
 };
