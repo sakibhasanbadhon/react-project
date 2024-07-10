d030879acb752ace29fde68ada10b34ae4217432
sakib <badhonkhan2033@gmail.com>
2024-07-10 23:58:12 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const App = () => {
 
-  let [list,setList]= useState([]);
-  let [item,setItem]= useState("");
+  let [data,setData]= useState();
 
-   const addToList = () =>{
-      list.push(item);
-      setList([...list])
-      
-   }
-
-   const removeItem= (index) =>{
-      list.splice(index,1);
-      setList([...list])
-
-   }
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(res=>res.json())
+    .then(json=>setData(json))
+  },[])
 
   return (
     <div>
-      <table>
-        <tbody>
-          {
-            list.length !==0?(
-              list.map((element, index)=>{
-                return(
-                  <tr>
-                    <td>{element}</td>
-                    <td><button onClick={()=>{
-                      removeItem(index)
-                    }}>X</button></td>
-                  </tr>
-                )
-              })
-            ):(<tr></tr>)
-          }
-        </tbody>
-      </table>
-      <input onChange={(e)=>setItem(e.target.value)} type="text" placeholder="item"/>
-      <button onClick={addToList}> Add </button>
+      {JSON.stringify(data)}
+        
     </div>
   );
 };
