16e7cc9f09407a57cb29f786b9c350a7143e156d
sakib <badhonkhan2033@gmail.com>
2024-07-14 22:21:32 +0600

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useEffect, useState } from "react";
 
-
+import Header from './assets/component/Header';
+import Hero from './assets/component/Hero';
 const App = () => {
-
-  let [data,setData]= useState();
-
-  useEffect(()=>{
-    fetch('https://dummyjson.com/products')
-    .then(res=>res.json())
-    .then(json=>setData(json))
-  },[])
 
   return (
     <div>
-      {JSON.stringify(data)}
+      <BrowserRoute>
+        <Routes>
+          <Routes path="/" element={<Header/>} />
+          <Routes path="/hero" element={<Hero/>} />
+        </Routes>
+      </BrowserRoute>
         
     </div>
   );
